1034c0bba63f9e96e6a2c269fcb6f112570c6a95
Cormanz <mrccay12@gmail.com>
2023-02-14 23:18:46 -0500

    restructuring and en passant

--- src/tests/boards/mod.rs
+++ src/tests/boards/mod.rs
@@ -1,14 +1,32 @@
-use crate::boards::{Board, perft};
+use crate::{boards::{perft, Board}, communication::UCICommunicator};
 
+#[ignore]
 #[test]
 fn startpos_perft() {
-    let mut board = Board::load_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-    
+    let mut uci = UCICommunicator {
+        board: Board::load_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
+    };
+
     // Perft Results sourced on WebPerft (https://analog-hors.github.io/webperft/)
 
-    assert_eq!(perft(&mut board, 1, 0), 20, "Perft Test (depth = 1)");
-    assert_eq!(perft(&mut board, 2, 0), 400, "Perft Test (depth = 2)");
-    assert_eq!(perft(&mut board, 3, 0), 8902, "Perft Test (depth = 3)");
-    assert_eq!(perft(&mut board, 4, 0), 197281, "Perft Test (depth = 4)");
-    assert_eq!(perft(&mut board, 5, 0), 4865609, "Perft Test (depth = 5)");
+    assert_eq!(perft(&mut uci, 1, 0, None), 20, "Perft Test (depth = 1)");
+    assert_eq!(perft(&mut uci, 2, 0, None), 400, "Perft Test (depth = 2)");
+    assert_eq!(perft(&mut uci, 3, 0, None), 8902, "Perft Test (depth = 3)");
+    assert_eq!(perft(&mut uci, 4, 0, None), 197281, "Perft Test (depth = 4)");
+    assert_eq!(perft(&mut uci, 5, 0, None), 4865609, "Perft Test (depth = 5)");
+    //assert_eq!(perft(&mut uci, 6, 0, None), 119060324, "Perft Test (depth = 6)");
+}
+
+
+#[test]
+fn a4_b5_Nf3_perft() {
+    
+    let mut uci = UCICommunicator {
+        board: Board::load_fen("rnbqkbnr/p1pppppp/8/1P6/8/8/1PPPPPPP/RNBQKBNR")
+    };
+    uci.board.print_board();
+
+    // Perft Results sourced on WebPerft (https://analog-hors.github.io/webperft/)
+
+    assert_eq!(perft(&mut uci, 2, 0, None), 502, "Perft Test (depth = 3)");
 }

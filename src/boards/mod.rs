348d48494a41dff1f95c985a6e3458cabf15dc1e
Cormanz <mrccay12@gmail.com>
2023-02-17 13:41:14 -0500

    Zobrist Hashing

--- src/boards/mod.rs
+++ src/boards/mod.rs
@@ -1,7 +1,9 @@
 mod board;
 mod movegen;
 mod perft;
+mod zobrist;
 
 pub use board::*;
 pub use movegen::*;
 pub use perft::*;
+pub use zobrist::*;
---
+++ src/boards/zobrist.rs
@@ -0,0 +1,32 @@
+use rand::prelude::*;
+
+use crate::boards::Board;
+
+pub fn generate_zobrist(pieces: i16, teams: i16, positions: i16) -> Vec<usize> {
+    let mut rng = rand_hc::Hc128Rng::from_entropy();
+    let len = ((pieces * teams) + 2) * positions;
+
+    let mut zobrist: Vec<usize> = Vec::with_capacity(len as usize);
+    for i in 0..len {
+        zobrist.insert(i as usize, rng.next_u64() as usize);
+    }
+
+    zobrist
+}
+
+pub fn hash_board(board: &Board, moving_team: i16, zobrist: &Vec<usize>) -> usize {
+    let mut hash: usize = 0;
+    let mut ind: i16 = 0;
+    let positions = board.row_gap * board.col_gap;
+    hash ^= zobrist[moving_team as usize];
+    for piece in &board.state {
+        let piece = *piece;
+        let first_move = board.pieces.iter()
+            .find(|piece| piece.pos == ind)
+            .unwrap().first_move;
+        hash ^= zobrist[(ind + positions * if first_move { 1 } else { 0 } + (positions * 2) * piece) as usize];
+        ind += 1;
+    }
+
+    return hash;
+}

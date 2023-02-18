d5eeee5f15aa1ceccf25f43abb2a7609347ca13f
Cormanz <mrccay12@gmail.com>
2023-02-18 00:27:40 -0500

    Iterative Deepening

--- src/engine/search.rs
+++ src/engine/search.rs
@@ -1,5 +1,25 @@
 use crate::boards::{Board, Action, GameResult};
-use super::{MIN_VALUE, evaluate, SearchInfo, MAX_VALUE};
+use super::{MIN_VALUE, evaluate, SearchInfo, MAX_VALUE, get_epoch_ms};
+
+pub fn root_search(search_info: &mut SearchInfo, board: &mut Board, starting_team: i16, max_time: u128) -> i32 {
+    let mut total_time = 0;
+    let mut depth = 1;
+    loop {
+        let start = get_epoch_ms();
+        search_info.root_depth = depth;
+        let score = search(search_info, board, MIN_VALUE, MAX_VALUE, depth, 0, starting_team);
+        let end = get_epoch_ms();
+        let time = end - start;
+        total_time += time;
+
+        search_info.time = total_time;
+        if total_time >= max_time {
+            return score;
+        }
+
+        depth += 1;
+    }
+}
 
 pub fn search(search_info: &mut SearchInfo, board: &mut Board, mut alpha: i32, beta: i32, depth: i16, ply: i16, starting_team: i16) -> i32 {
     if depth == 0 {
@@ -10,13 +30,13 @@ pub fn search(search_info: &mut SearchInfo, board: &mut Board, mut alpha: i32, b
 
     match board.win_conditions.duplicate().compute(board, &actions) {
         GameResult::Win => {
-            return MAX_VALUE - 100 + (depth as i32); // Higher Depth should mean a faster win
+            return MAX_VALUE + (depth as i32); // Higher Depth should mean a faster win
         }
         GameResult::Draw => {
             return 0;
         }
         GameResult::Lose => {
-            return MIN_VALUE + 100 - (depth as i32); // Lower Depth should mean a slower loss
+            return MIN_VALUE - (depth as i32); // Lower Depth should mean a slower loss
         }
         GameResult::Ongoing => {}
     }
--- src/engine/util.rs
+++ src/engine/util.rs
@@ -1,10 +1,18 @@
 use crate::boards::Action;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 // I like to live dangerously on the edge.
-pub const MIN_VALUE: i32 = -2147483647;
-pub const MAX_VALUE: i32 = 2147483647;
-
+pub const MIN_VALUE: i32 = -2_000_000_000;
+pub const MAX_VALUE: i32 = 2_000_000_000;
 pub struct SearchInfo {
     pub root_depth: i16,
+    pub time: u128,
     pub best_move: Option<Action>
+}
+
+pub fn get_epoch_ms() -> u128 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis()
 }

a452a5d1fa61e30ac650680c5b9c15adad13c29f
Cormanz <mrccay12@gmail.com>
2023-02-20 12:15:27 -0500

    Addition of perft_psuedolegal and benchmarks

--- src/boards/perft.rs
+++ src/boards/perft.rs
@@ -2,6 +2,23 @@ use crate::communication::{Communicator, UCICommunicator};
 
 use super::{Action, Board};
 
+pub fn perft_psuedolegal(uci: &mut UCICommunicator, depth: i16, last_action: Option<Action>) -> u64 {
+    let mut nodes: u64 = 0;
+
+    let actions = uci.board.generate_legal_moves();
+    if depth == 1 {
+        return actions.len() as u64;
+    }
+
+    for action in &actions {
+        uci.board.make_move(*action);
+        nodes += perft(uci, depth - 1, Some(*action));
+        uci.board.undo_move();
+    }
+
+    nodes
+}
+
 pub fn perft(uci: &mut UCICommunicator, depth: i16, last_action: Option<Action>) -> u64 {
     let mut nodes: u64 = 0;
 
---
+++ src/tests/boards/bench.rs
@@ -0,0 +1,29 @@
+use std::fmt::Debug;
+
+use crate::{boards::{Board, perft_psuedolegal, perft}, engine::get_epoch_ms};
+
+pub fn bench<T : Debug>(benchmark: &str, run: &dyn Fn() -> T) {
+    let start = get_epoch_ms();
+    let result = run();
+    let end = get_epoch_ms();
+    println!("{} | result: {:?} time: {}", benchmark, result, end - start);
+}
+
+pub fn psuedolegal_moves() {
+    bench("perft psuedolegal", &|| {
+        let mut uci = Board::load_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - -");
+        perft_psuedolegal(&mut uci, 5, None)
+    });
+}
+
+pub fn legal_moves() {
+    bench("perft legal", &|| {
+        let mut uci = Board::load_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - -");
+        perft(&mut uci, 5, None)
+    });
+}
+
+pub fn boards_bench() {
+    psuedolegal_moves();
+    legal_moves();
+}
